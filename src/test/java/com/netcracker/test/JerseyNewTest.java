25c65f5e9f663ff150cb5d6a3d6149b66b615432
Fabrizio Cucci <fabrizio.cucci@ie.ibm.com>
2016-07-22 22:22:10 +0100

    Updated test to reflect my issue
    Here I'm using a parallel Stream to issue simultaneous requests and I'm creating a new Jersey client for each thread.

--- src/test/java/com/netcracker/test/JerseyNewTest.java
+++ src/test/java/com/netcracker/test/JerseyNewTest.java
@@ -1,37 +1,29 @@
 package com.netcracker.test;
 
-import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.client.Entity.*;
 
 import java.io.FileInputStream;
 import java.security.KeyStore;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation.Builder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.SslConfigurator;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
 public class JerseyNewTest {
 	
-	private static final Object mutex = new Object();
-	private static final AtomicInteger count = new AtomicInteger(0);
 	private static final String URL = "https://localhost:8443/";
 	
 	private static KeyStore trustStore;
-
-	private Client client;
 	
 	@BeforeClass
 	public static void setupTrustStore() throws Exception {
@@ -39,119 +31,36 @@ public class JerseyNewTest {
     	trustStore.load(new FileInputStream("miko0516.trust.jks"), "miko0516".toCharArray());
 	}
 
-	@Before
-	public void setupClient() {
-    	client = constructClient(trustStore);
-    	count.set(0);
-	}
-	
-	@After
-	public void closeClient() {
-		client.close();
-	}
-	
 	@Test
-	@Ignore
-	public void testSingle() throws Exception {
-		System.out.println("START single TEST");
-        WebTarget target = client.target(URL);
-        Builder builder = target.request();
-    	Response response = builder.post(entity("{\"requestContent\" : \"qweAsd\"}", MediaType.APPLICATION_JSON_TYPE));
-    	assert response != null;
-    	System.out.println(response.getStatusInfo().getStatusCode() + " " + response.getStatusInfo().getReasonPhrase());
-    	response.readEntity(String.class);
-    	response.close();
-    	System.out.println("FINSH single TEST");
+	public void testMultipleRequestWithParallelStream() {
+		IntStream.range(0, 10)
+			.parallel() // if this is commented, everything looks good!
+			.mapToObj(i -> constructClient(trustStore).target(URL))
+			.forEach(this::doRequest);
+	}
+	
+	private static Client constructClient(KeyStore trustStore) {
+		return ClientBuilder.newBuilder()
+				.sslContext(newSslContext())
+				.register(JacksonJsonProvider.class)
+				.build();
 	}
 
-	@Test
-	@Ignore
-	public void testMultithread3() throws Exception {
-		testMultithread(3);
+	private static SSLContext newSslContext() {
+		return SslConfigurator.newInstance()
+				.trustStore(trustStore)
+				.createSSLContext();
 	}
 	
-	@Test
-	public void testMultithread10() throws Exception {
-		testMultithread(10);
-	}
-	
-	@Test
-	@Ignore
-	public void testMultithread50() throws Exception {
-		testMultithread(50);
-	}
-	
-	public void testMultithread(int size) throws Exception {
-    	System.out.println("START with " + size + " THREADS");
-		for (int i = 0; i < size; i++) {
-        	new Thread(new RunnerThread(client)).start();
-        }
-        synchronized (mutex) {
-        	while (count.get() != size) {
-        		mutex.wait(10);
-        	}
-        	mutex.notifyAll(); // let's start!
-		}
-        synchronized (mutex) {
-        	while (count.get() != 0) {
-        		mutex.wait(10);
-        	}
-		}
-    	System.out.println("ALL " + size + " FINISHED");
-	}
-
-	protected static Client constructClient(KeyStore trustStore) {
-		
-		SslConfigurator sslConfig = SslConfigurator.newInstance().trustStore(trustStore);
-		SSLContext sslContext = sslConfig.createSSLContext();
-
-		
-		ClientBuilder builder = ClientBuilder.newBuilder();
-    	builder.sslContext(sslContext);
-    	
-    	Client client = builder.build();
-    	client.register(JacksonJsonProvider.class);
-    	return client;
-	}
-
-    static class RunnerThread implements Runnable {
-		final Client client;
-		private Builder builder;
-		private String name;
-    	
-    	public RunnerThread(Client client) {
-    		this.client = client;
-		}
-    	
-    	@Override
-    	public void run() {
-    		name = Thread.currentThread().getName();
-	        WebTarget target = client.target(URL);
-	        builder = target.request();
-	        synchronized (mutex) {
-		        count.incrementAndGet();
-	        	try {
-					mutex.wait();
-				} catch (InterruptedException e) {
-				}
-			}
-			doRequest();
-    		count.decrementAndGet();
-    		synchronized (mutex) {
-    			mutex.notify();
-			}
+	private void doRequest(WebTarget webTarget) {
+		try {
+        	Response response = webTarget.request().post(entity("{\"requestContent\" : \"qweAsd\"}", MediaType.APPLICATION_JSON_TYPE));
+        	System.out.println("\t" + response.getStatusInfo().getStatusCode() + " " + response.getStatusInfo().getReasonPhrase());
+        	response.readEntity(String.class);
+        	response.close();
+    	} catch (Exception e) {
+    		System.out.println("\t" + e.getMessage());
     	}
-
-		protected void doRequest() {
-			try {
-	        	Response response = builder.post(entity("{\"requestContent\" : \"qweAsd\"}", MediaType.APPLICATION_JSON_TYPE));
-	        	assert response != null;
-	        	System.out.println(name + "\t" + response.getStatusInfo().getStatusCode() + " " + response.getStatusInfo().getReasonPhrase());
-	        	response.readEntity(String.class);
-	        	response.close();
-        	} catch (Exception e) {
-        		System.out.println(name + "\t" + e.getMessage());
-        	}
-		}
-    }
+	}
+	
 }
